8c78b2e16d6475f4f4ff3e48540e38b55701ed27
Stephan Roth <author@clean-cpp.com>
2017-04-01 18:35:35 +0200

    1st TDD cycle completed.

--- RomanNumeralsKata/ArabicToRomanNumeralsConverterTestCase.cpp
+++ RomanNumeralsKata/ArabicToRomanNumeralsConverterTestCase.cpp
@@ -1,10 +1,15 @@
 #include <gtest/gtest.h>
+#include <string>
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
 
-TEST(ArabicToRomanNumeralsConverterTestCase, PreparationsCompleted) {
-  GTEST_SUCCEED();
+std::string convertArabicNumberToRomanNumeral(const unsigned int arabicNumber) {
+  return "I";
+}
+
+TEST(ArabicToRomanNumeralsConverterTestCase, 1_isConvertedTo_I) {
+  ASSERT_EQ("I", convertArabicNumberToRomanNumeral(1));
 }
