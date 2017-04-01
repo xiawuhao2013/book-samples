058eabe8296f09fee8dfab5d9565e426fe3d2e1f
Stephan Roth <author@clean-cpp.com>
2017-04-01 19:06:19 +0200

    2nd TDD cycle completed.

--- RomanNumeralsKata/ArabicToRomanNumeralsConverterTestCase.cpp
+++ RomanNumeralsKata/ArabicToRomanNumeralsConverterTestCase.cpp
@@ -7,9 +7,16 @@ int main(int argc, char** argv) {
 }
 
 std::string convertArabicNumberToRomanNumeral(const unsigned int arabicNumber) {
+  if (arabicNumber == 2) {
+    return "II";
+  }
   return "I";
 }
 
 TEST(ArabicToRomanNumeralsConverterTestCase, 1_isConvertedTo_I) {
   ASSERT_EQ("I", convertArabicNumberToRomanNumeral(1));
+}
+
+TEST(ArabicToRomanNumeralsConverterTestCase, 2_isConvertedTo_II) {
+  ASSERT_EQ("II", convertArabicNumberToRomanNumeral(2));
 }
