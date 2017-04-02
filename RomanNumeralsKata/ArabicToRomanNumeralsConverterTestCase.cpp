e300b26c98122779900521ca39d359917d0b6174
Stephan Roth <author@clean-cpp.com>
2017-04-02 17:07:33 +0200

    Added assertion class 'RomanNumeralAssert'

--- RomanNumeralsKata/ArabicToRomanNumeralsConverterTestCase.cpp
+++ RomanNumeralsKata/ArabicToRomanNumeralsConverterTestCase.cpp
@@ -19,6 +19,26 @@ std::string convertArabicNumberToRomanNumeral(unsigned int arabicNumber) {
   return romanNumeral;
 }
 
+// Test code starts here...
+
+class RomanNumeralAssert {
+public:
+  RomanNumeralAssert() = delete;
+  explicit RomanNumeralAssert(const unsigned int arabicNumber) :
+      arabicNumberToConvert(arabicNumber) { }
+  void isConvertedToRomanNumeral(const std::string& expectedRomanNumeral) const {
+    ASSERT_EQ(expectedRomanNumeral, convertArabicNumberToRomanNumeral(arabicNumberToConvert));
+  }
+
+private:
+  const unsigned int arabicNumberToConvert;
+};
+
+RomanNumeralAssert assertThat(const unsigned int arabicNumber) {
+  RomanNumeralAssert assert { arabicNumber };
+  return assert;
+}
+
 TEST(ArabicToRomanNumeralsConverterTestCase, 1_isConvertedTo_I) {
   ASSERT_EQ("I", convertArabicNumberToRomanNumeral(1));
 }
@@ -41,5 +61,9 @@ TEST(ArabicToRomanNumeralsConverterTestCase, 20_isConvertedTo_XX) {
 
 TEST(ArabicToRomanNumeralsConverterTestCase, 30_isConvertedTo_XXX) {
   ASSERT_EQ("XXX", convertArabicNumberToRomanNumeral(30));
+}
+
+TEST(ArabicToRomanNumeralsConverterTestCase, 33_isConvertedTo_XXXIII) {
+  assertThat(33).isConvertedToRomanNumeral("XXXIII");
 }
 
